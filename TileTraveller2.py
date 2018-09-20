e9fc719c17c02ce83870a118973f07272a5a4b29
unknown <onyth23@gmail.com>
2018-09-20 15:03:38 +0000

    bug fixes to get passed on mimir

--- TileTraveller2.py
+++ TileTraveller2.py
@@ -12,7 +12,7 @@
 
 x,y=1,1
 Play = True
-inv_dir = "Not a valid direction"
+inv_dir = "Not a valid direction!"
 
 
 #Recives input of 1 or 0 depending on if you can travel to that direction. N E S W.
@@ -32,7 +32,7 @@ def can_travel(n, e, s, w):
 
     for i in range(1, len(dirs), 1):
         s += " or " + dirs[i]
-    return s
+    return s + "."
 
 
 #Prompts user for a direction and returns it
@@ -41,61 +41,71 @@ def get_move():
     u = u.lower()
     return u
 
+
 #Main game loop. Plays while True. Only False case is when user enteres x,y cords 3,1
+
+print(can_travel(1,0,0,0))
+
 while Play:
     if x == 1 and y == 1:
-        print(can_travel(1,0,0,0))
         move = get_move()
         if move == 'n':
             x,y = 1,2
+            print(can_travel(1,1,1,0))
         else:
             print(inv_dir)
+
     elif x ==  1 and y == 2:
-        print(can_travel(1,1,1,0))
         move = get_move()
         if move == "n":
             x,y=1,3
+            print(can_travel(0,1,1,0))
         elif move == "e":
             x,y=2,2
+            print(can_travel(0,0,1,1))
         elif move == "s":
             x,y=1,1
+            print(can_travel(1,0,0,0))
         else:
             print(inv_dir)
     elif x ==  1 and y == 3:
-        print(can_travel(0,1,1,0))
         move = get_move()
         if move == "e":
             x,y=2,3
+            print(can_travel(0,1,0,1))
         elif move == "s":
             x,y=1,2
+            print(can_travel(1,1,1,0))
         else:
             print(inv_dir)
 
     elif x ==  2 and y == 1:
-        print(can_travel(1,0,0,0))
         move = get_move()
         if move == "n":
             x,y=2,2
+            print(can_travel(0,0,1,1))
         else:
             print(inv_dir)
 
     elif x ==  2 and y == 2:
-        print(can_travel(0,0,1,1))
         move = get_move()
         if move == "w":
             x,y=1,2
+            print(can_travel(1,1,1,0))
         elif move == "s":
             x,y=2,1
+            print(can_travel(1,0,0,0))
         else:
             print(inv_dir)
 
     elif x ==  2 and y == 3:
-        print(can_travel(0,1,0,1))
         move = get_move()
         if move == "w":
             x,y=1,3
+            print(can_travel(0,1,1,0))
         elif move == "e":
             x,y=3,3
+            print(can_travel(0,0,1,1))
         else:
             print(inv_dir)
 
@@ -105,21 +115,22 @@ while Play:
         Play = False
 
     elif x ==  3 and y == 2:
-        print(can_travel(1,0,1,0))
         move = get_move()
         if move == "n":
             x,y=3,3
+            print(can_travel(0,0,1,1))
         elif move == "s":
             x,y=3,1
         else:
             print(inv_dir)
 
     elif x ==  3 and y == 3:
-        print(can_travel(0,0,1,1))
         move = get_move()
         if move == "w":
             x,y=2,3
+            print(can_travel(0,1,0,1))
         elif move == "s":
             x,y=3,2
+            print(can_travel(1,0,1,0))
         else:
             print(inv_dir)
